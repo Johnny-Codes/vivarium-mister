b77953dd92a87b5a98e4ffe2e2f95d8832ca3ab4
pjohns <johnspaulm@gmail.com>
2024-07-17 22:22:06 -0500

    cleaned up code

--- mister_cron.c
+++ mister_cron.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <bcm2835.h>
 
-#define BUTTON_PIN RPI_V2_GPIO_P1_11  // GPIO 17
 #define RELAY_PIN  RPI_V2_GPIO_P1_13  // GPIO 27
 
 int main(int argc, char **argv) {
+    
     if (!bcm2835_init()) {
         printf("BCM2835 init failed");
         return 1;
     }
 
-
     // set relay to output
     bcm2835_gpio_fsel(RELAY_PIN, BCM2835_GPIO_FSEL_OUTP);
+    // turn relay/pump on
     bcm2835_gpio_write(RELAY_PIN, HIGH);
+    // run for 5 seconds
     bcm2835_delay(5000);
+    // turn off relay/pump
     bcm2835_gpio_write(RELAY_PIN, LOW);
+    // safe close
     bcm2835_close();
+    
     return 0;
 }
-
-
-
-
-
